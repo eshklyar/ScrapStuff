b0e17452b7368c9b8ed87b30e0907feff9598929
eshklyar <eshklyar@users.noreply.github.com>
2024-05-30 15:01:04 -0500

    trying to fix fib2 agin again

--- fib2.py
+++ fib2.py
@@ -1,5 +1,37 @@
 import math
 
+# def fibonacci(n: int) -> int:
+#     fib_values = [0, 1] + [0] * (n - 1)
+#
+#     for i in range(2, n + 1):
+#         fib_values[i] = fib_values[i - 1] + fib_values[i - 2]
+#
+#     return fib_values[n]
+
+
+
+from typing import Dict, Optional
+
+
+# def fibonacci(n: int, recursive_hash: Optional[Dict[int, int]] = None) -> int:
+#     if recursive_hash is None:
+#         recursive_hash = {}
+#
+#     if n in recursive_hash:
+#         return recursive_hash[n]
+#
+#     if n <= 1:
+#         return n
+#
+#     recursive_hash[n] = fibonacci(n - 1, recursive_hash) + fibonacci(n - 2, recursive_hash)
+#
+#     return recursive_hash[n]
+#
+#
+# n = 4
+# f = fibonacci(n)
+# print(f)  # Output: 3
+
 
 def fibonacci(n: int, recursive_hash: dict = None) -> int:
     if recursive_hash is None:
