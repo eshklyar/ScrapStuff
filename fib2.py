70c6abcdcd0f2d0bd2167e8ed22d827bb149330e
eshklyar <eshklyar@users.noreply.github.com>
2024-05-30 14:54:37 -0500

    trying to fix fib2

--- fib2.py
+++ fib2.py
@@ -1,16 +1,16 @@
 import math
 
 
-# def fibonacci(n: int, recursive_hash: dict = None) -> int:
-#     if recursive_hash is None:
-#         recursive_hash = {0:0,1:1}
-#     if n in recursive_hash:
-#         return recursive_hash[n]
-#     # if n <= 1:
-#     #     return n
-#     # for i in range(2,n):
-#     recursive_hash[n] = fibonacci(n-1,recursive_hash) + fibonacci(n-2, recursive_hash)
-#     return recursive_hash[n]
+def fibonacci(n: int, recursive_hash: dict = None) -> int:
+    if recursive_hash is None:
+        recursive_hash = {0:0,1:1}
+    if n in recursive_hash:
+        return recursive_hash[n]
+    # if n <= 1:
+    #     return n
+    # for i in range(2,n):
+    recursive_hash[n] = fibonacci(n-1,recursive_hash) + fibonacci(n-2, recursive_hash)
+    return recursive_hash[n]
 
 
 def find_prod(i: int) ->(int, int, bool):
@@ -18,16 +18,7 @@ def find_prod(i: int) ->(int, int, bool):
     n = 1
     fib_list = [0,1]
 
-    def fibonacci(n: int, recursive_hash: dict = None) -> int:
-        if recursive_hash is None:
-            recursive_hash = {0: 0, 1: 1}
-        if n in recursive_hash:
-            return recursive_hash[n]
-        # if n <= 1:
-        #     return n
-        # for i in range(2,n):
-        recursive_hash[n] = fibonacci(n - 1, recursive_hash) + fibonacci(n - 2, recursive_hash)
-        return recursive_hash[n]
+
     while fib_list[n] <= si:
         n += 1
         fib_list.append(fibonacci(n))
